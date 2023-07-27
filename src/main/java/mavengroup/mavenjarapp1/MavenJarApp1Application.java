6fe46d07ffd254874321dbd92dd0fae85c3a0ffe
mallikaraju <marri.mallika@gmail.com>
2023-07-27 21:23:03 +0530

    first commit

---
+++ src/main/java/mavengroup/mavenjarapp1/MavenJarApp1Application.java
@@ -0,0 +1,13 @@
+package mavengroup.mavenjarapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJarApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJarApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/mavengroup/mavenjarapp1/MavenJarApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mavengroup.mavenjarapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJarApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
